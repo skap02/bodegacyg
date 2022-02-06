bb13ccc7edde50472e632399a777b80bf06a7e25
Jean Pierre <98369471+skap02@users.noreply.github.com>
2022-02-05 19:59:40 -0500

    Add files via upload

---
+++ santanaturahuancayo/public_html/index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preload" href="css/normalize.css" as="style">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link href="https://fonts.googleapis.com/css2?family=Birthstone&family=Festive&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Anton&family=Birthstone&family=Festive&display=swap" rel="stylesheet">
+    <title>Directv Agente Autorizado</title>
+    <link rel="preload" href="css/style.css" as="style">
+    <link href="css/style.css" rel="stylesheet">
+</head>
+<body herf= "img/uno.jpg" >
+    <header class="dtv">
+        <img class="logo" src="img/directvlogo.png">
+    </header>
+
+    <!--   OCULTAR CODIGO  -->
+<div class="nav-bg">
+    <nav class="navegacion-principal contenedor">
+        <a href="#">Descripción</a>
+        <a href="#">Servicios</a>
+        <a href="#">Contacto</a>
+        <a href="#">Guia de Canales</a>
+
+    </nav>
+</div>
+
+    <section class="tienda">
+        <div class="contenido-tienda">
+        <a class="boton" href="">facebook</a>
+        <a class="boton" href="">whatsapp</a>
+
+        </div>
+    </section>
+
+    <main class="contenedor sombra">
+        <br>
+        <h2 class="titulo2">Servicios</h2>
+
+        <div class="ofertas">
+    
+        <section class="productos">
+            <h3>Post Pago</h3>
+            <div class="img">
+                <img class="imagen" src="img/11.jpg">
+            </div>
+            <a class="boton" href="">Solicitar</a>
+        </section>
+        
+        <section class="productos">
+            <h3>Pre Pago</h3>
+            <div class="img">
+                <img class="imagen2" src="img/22.jpg">
+            </div>
+            <a class="boton" href="">solicitar</a>
+        </section>
+        
+        <section class="productos">
+            <h3>Servicio Técnico</h3>
+            <div class="img">
+                <img class="imagen3" src="img/33.jpg">
+            </div>
+            <a class="boton" href="">solicitar</a>
+        </section>
+    </div> 
+    
+        <section>
+            <h2 class="titulo2">Contacto</h2>
+
+            <form class="formulario">
+                <fieldset>
+                    <legend>Rellene el Formulario</legend> 
+                <div class="contenedor-campos">
+                    <div class="campos">
+                        <label>Nombre</label>
+                        <input class="input-text" type="text" placeholder="Tu Nombre">
+                    </div>
+                    
+                    <div class="campos">
+                        <label>Telefono</label>
+                        <input class="input-text" type="tel" placeholder="Tu Teléfono">
+                    </div>
+                    
+                    <div class="campos">
+                        <label>Correo</label>
+                        <input class="input-text" type="email" placeholder="Tu correo">
+                    </div>
+                    
+                    <div class="campos">
+                        <label>Dirección donde desea la instalación</label>
+                        <input class="input-text" type="email" placeholder="Tu correo">
+                    </div>
+
+                    <div class="campos">
+                        <label>Mensaje</label>
+                        <textarea class="input-text"></textarea>
+                    </div>
+                </div> <!--contenedor de campos-->
+            
+                <div class="alinear-derecha flex">
+                    <input class="boton w-sm-100" type="submit" value="Enviar">
+                </div>
+               
+                </fieldset>
+            </form>
+            <br>
+        </section>
+    </main>  
+    <footer>
+        <p>EL FIN JUSTIFICA LOS MEDIOS</p>
+    </footer>  
+</body>
+</html>
