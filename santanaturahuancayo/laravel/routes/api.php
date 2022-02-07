456e6be2df5bc07a6f578e90e8e82af949d1f62c
EderRoman <i2115665@continental.edu.pe>
2022-02-06 22:14:29 -0500

    Update del laravel

--- santanaturahuancayo/laravel/routes/api.php
+++ santanaturahuancayo/laravel/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\v1\ProductosController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get("/v1/productos",[ProductosController::class,"getAll"]);
+Route::get("/v1/productos/{id}",[ProductosController::class,"getItem"]);
+Route::post("/v1/productos",[ProductosController::class,"store"]);
+Route::put("/v1/productos",[ProductosController::class,"update"]);
+Route::patch("/v1/productos",[ProductosController::class,"patch"]);
+Route::delete("/v1/productos{id}",[ProductosController::class,"delete"])
