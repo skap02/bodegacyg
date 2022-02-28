5f2adb21c4845e8a6f3ad3b7a0183b20b29dcb80
EderRoman <i2115665@continental.edu.pe>
2022-02-27 22:29:46 -0500

    Modificaciones

--- bodegacyg/laravel/routes/api.php
+++ bodegacyg/laravel/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\v1\ProductoController;
+use App\Http\Controllers\v1\CategoriaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,9 +20,20 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+//Para llamar productos
+
 Route::get("/v1/producto",[ProductoController::class,"getAll"]);
 Route::get("/v1/producto/{id}",[ProductoController::class,"getItem"]);
 Route::post("/v1/producto",[ProductoController::class,"store"]);
 Route::put("/v1/producto",[ProductoController::class,"update"]);
 Route::patch("/v1/producto/{id}",[ProductoController::class,"patch"]);
 Route::delete("/v1/producto/{id}",[ProductoController::class,"delete"]);
+
+//Para llamar categorias
+
+Route::get("/v1/categoria",[CategoriaController::class,"getAll"]);
+Route::get("/v1/categoria/{id}",[CategoriaController::class,"getItem"]);
+Route::post("/v1/categoria",[CategoriaController::class,"store"]);
+Route::put("/v1/categoria",[CategoriaController::class,"update"]);
+Route::patch("/v1/categoria/{id}",[CategoriaController::class,"patch"]);
+Route::delete("/v1/categoria/{id}",[CategoriaController::class,"delete"]);
